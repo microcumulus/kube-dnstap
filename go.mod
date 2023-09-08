d0464dfce53cbbe29f5d29d9ff4d24de2ec0734e
Andrew Stuart <andrew.stuart2@gmail.com>
2023-09-08 10:32:07 -0700

    Implemented

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.21.0
 
 require (
 	github.com/dnstap/golang-dnstap v0.4.0
+	github.com/gopuff/morecontext v0.4.1
 	github.com/sirupsen/logrus v1.9.3
+	google.golang.org/protobuf v1.28.1
 	k8s.io/api v0.27.4
 	k8s.io/client-go v0.27.4
 )
@@ -40,7 +42,6 @@ require (
 	golang.org/x/text v0.8.0 // indirect
 	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"log"
 	"net"
 	"os"
-	"os/signal"
 	"path/filepath"
+	"reflect"
 	"sync"
-	"syscall"
 
 	dnstap "github.com/dnstap/golang-dnstap"
+	"github.com/gopuff/morecontext"
+	"github.com/miekg/dns"
 	"github.com/sirupsen/logrus"
+	"google.golang.org/protobuf/proto"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
@@ -23,61 +25,113 @@ import (
 const gb = 1024 * 1024 * 1024
 
 func main() {
-	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, os.Interrupt)
-	defer cancel()
+	ctx := morecontext.ForSignals()
+	// logrus.SetFormatter(&logrus.JSONFormatter{})
 
 	l, err := net.Listen("tcp", "0.0.0.0:1234")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_ = k8sMap(ctx)
+	m := k8sMap(ctx)
 
 	i := dnstap.NewFrameStreamSockInput(l)
 	bch := make(chan []byte)
 	go i.ReadInto(bch)
 
-	for bs := range bch {
-		r, err := dnstap.NewReader(bytes.NewReader(bs), nil)
-		if err != nil {
-			logrus.WithError(err).Error("failed to create dnstap reader")
-			continue
-		}
-		d := dnstap.NewDecoder(r, gb)
-		f := &dnstap.Dnstap{}
-		err = d.Decode(f)
-		if err != nil {
-			logrus.WithError(err).Error("failed to decode dnstap message")
-			continue
-		}
-		log.Println(f.String())
-	}
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case bs := <-bch:
+			var f dnstap.Dnstap
+			err := proto.Unmarshal(bs, &f)
+			if err != nil {
+				logrus.WithError(err).Error("failed to decode dnstap message")
+				continue
+			}
+			addr := net.IP(f.Message.QueryAddress)
 
+			v, ok := m.Load(addr.String())
+			if !ok {
+				logrus.WithFields(logrus.Fields{
+					"addr": addr,
+				}).Error("no pod found")
+				continue
+			}
+			pod, ok := v.(*corev1.Pod)
+			if !ok {
+				logrus.WithField("addr", addr).WithField("type", reflect.TypeOf(v)).Error("cached value is not a pod")
+				continue
+			}
+			lg := logrus.WithFields(logrus.Fields{
+				"pod": pod.Name,
+				// "addr": addr,
+				// "tap":  f,
+			})
+			lg.Debug("dnstap message received")
+
+			msgBs := f.Message.GetQueryMessage()
+			if len(msgBs) == 0 {
+				msgBs = f.Message.GetResponseMessage()
+			}
+			if len(msgBs) == 0 {
+				lg.Error("no dns message found")
+				continue
+			}
+			var msg dns.Msg
+			err = msg.Unpack(msgBs)
+			if err != nil {
+				logrus.WithError(err).Error("failed to decode dns message")
+				continue
+			}
+
+			lg.WithField("type", f.Message.Type.String()).WithField("msg", msg.Question[0].Name).Info()
+		}
+	}
 }
 
 func k8sMap(ctx context.Context) sync.Map {
 	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "home")
-	config, _ := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	clientset, _ := kubernetes.NewForConfig(config)
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		logrus.WithError(err).Fatal("failed to build config")
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		logrus.WithError(err).Fatal("failed to create clientset")
+	}
 	factory := informers.NewSharedInformerFactory(clientset, 0)
 	informer := factory.Core().V1().Pods().Informer()
 
 	var m sync.Map
 
-	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+	list, err := clientset.CoreV1().Pods("").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		logrus.WithError(err).Fatal("failed to list pods")
+	}
+	for _, pod := range list.Items {
+		pod := pod
+		m.Store(pod.Status.PodIP, &pod)
+	}
+
+	_, err = informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			pod := obj.(*corev1.Pod)
-			m.Store(pod.Status.PodIP, &pod)
+			m.Store(pod.Status.PodIP, pod)
 		},
 		UpdateFunc: func(oldObj, newObj interface{}) {
 			newPod := newObj.(*corev1.Pod)
-			m.Store(newPod.Status.PodIP, &newPod)
+			m.Store(newPod.Status.PodIP, newPod)
 		},
 		DeleteFunc: func(obj interface{}) {
 			pod := obj.(*corev1.Pod)
 			m.Delete(pod.Status.PodIP)
 		},
 	})
+	if err != nil {
+		logrus.WithError(err).Fatal("failed to add event handler")
+	}
 
 	go informer.Run(ctx.Done())
 	return m
